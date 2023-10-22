2216a5b6909bdb3de627ce76d46a584166e36571
Issam Arabi <issam@odysseus.dgpsrv.sandbox>
2023-10-22 13:18:43 -0400

    refoactor IPD and add comments

--- jax_files/ipd_jax.py
+++ jax_files/ipd_jax.py
@@ -1,35 +1,74 @@
 import jax.numpy as jnp
 
-class IPD:
+class IteratedPrisonersDilemma:
     """
-    A two-agent vectorized environment.
+    A two-agent vectorized environment for the Iterated Prisoner's Dilemma (IPD).
     Possible actions for each agent are (C)ooperate and (D)efect.
+    
+    Attributes:
+    - reward_matrix: Matrix representing the rewards for each combination of actions.
+    - state_representations: One-hot encoded representation of possible states.
+    - initial_state: Initial state of the game.
     """
-    def __init__(self, init_state_coop=False, contrib_factor=1.33):
-        cc = contrib_factor - 1.
-        dd = 0.
-        dc = contrib_factor / 2. # I defect when opp coop
-        cd = contrib_factor / 2. - 1 # I coop when opp defect
-        self.payout_mat = jnp.array([[dd, dc],[cd, cc]])
-        # One hot state representation because this would scale to n agents
-        self.states = jnp.array([[[1, 0, 0, 1, 0, 0], #DD (WE ARE BACK TO THE REPR OF FIRST AGENT, SECOND AGENT)
-                                          [1, 0, 0, 0, 1, 0]], #DC
-                                         [[0, 1, 0, 1, 0, 0], #CD
-                                          [0, 1, 0, 0, 1, 0]]]) #CC
-        if init_state_coop:
-            self.init_state = jnp.array([0, 1, 0, 0, 1, 0])
+    
+    def __init__(self, start_with_cooperation=False, cooperation_factor=1.33):
+        """
+        Initialize the IPD environment.
+        
+        Args:
+        - start_with_cooperation: If True, the initial state is set to cooperation for both agents.
+        - cooperation_factor: Factor determining the reward for cooperation relative to defection.
+        """
+        reward_coop_coop = cooperation_factor - 1.
+        reward_defect_defect = 0.
+        reward_defect_coop = cooperation_factor / 2.  # Reward when I defect and opponent cooperates.
+        reward_coop_defect = cooperation_factor / 2. - 1  # Reward when I cooperate and opponent defects.
+        
+        # Define the reward matrix for the game.
+        self.reward_matrix = jnp.array([[reward_defect_defect, reward_defect_coop], 
+                                        [reward_coop_defect, reward_coop_coop]])
+        
+        # One-hot encoded representation of possible states.
+        self.state_representations = jnp.array([[[1, 0, 0, 1, 0, 0],  # Defect-Defect
+                                                 [1, 0, 0, 0, 1, 0]],  # Defect-Cooperate
+                                                [[0, 1, 0, 1, 0, 0],  # Cooperate-Defect
+                                                 [0, 1, 0, 0, 1, 0]]])  # Cooperate-Cooperate
+        
+        # Set the initial state.
+        if start_with_cooperation:
+            self.initial_state = jnp.array([0, 1, 0, 0, 1, 0])
         else:
-            self.init_state = jnp.array([0, 0, 1, 0, 0, 1])
+            self.initial_state = jnp.array([0, 0, 1, 0, 0, 1])
 
     def reset(self, unused_key):
-        return self.init_state, self.init_state
+        """
+        Reset the game to its initial state.
+        
+        Args:
+        - unused_key: Placeholder for a random seed (not used in this method).
+        
+        Returns:
+        - Tuple of initial states for both agents.
+        """
+        return self.initial_state, self.initial_state
 
-    def step(self, unused_state, ac0, ac1, unused_key):
-
-        r0 = self.payout_mat[ac0, ac1]
-        r1 = self.payout_mat[ac1, ac0]
-        state = self.states[ac0, ac1]
-        observation = state
-        reward = (r0, r1)
-        # State is observation in the IPD
-        return state, observation, reward, None
+    def step(self, unused_state, action_agent1, action_agent2, unused_key):
+        """
+        Execute a step in the game based on the actions of the two agents.
+        
+        Args:
+        - unused_state: Placeholder for the current state (not used in this method).
+        - action_agent1: Action taken by the first agent.
+        - action_agent2: Action taken by the second agent.
+        - unused_key: Placeholder for a random seed (not used in this method).
+        
+        Returns:
+        - Tuple containing the new state, observation, and rewards for both agents.
+        """
+        reward_agent1 = self.reward_matrix[action_agent1, action_agent2]
+        reward_agent2 = self.reward_matrix[action_agent2, action_agent1]
+        new_state = self.state_representations[action_agent1, action_agent2]
+        observation = new_state
+        rewards = (reward_agent1, reward_agent2)
+        
+        return new_state, observation, rewards, None
